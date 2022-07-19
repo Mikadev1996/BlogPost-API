cb921fade7dd7f5dfde2cd0b775df4429fdd984b
Mika.dev <charmikadev@gmail.com>
2022-07-19 22:01:13 +0100

    api & front end can run together fix

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const userRouter = require('./routes/users');
 
 const app = express();
 
@@ -12,21 +13,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
+app.use('/api/users', userRouter);
 
-app.get('/', (req, res, next) => {
-  res.send({
-    message: "test message"
-  })
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
 })
-
-app.get('/api', (req, res, next) => {
-  res.json({
-    "message": "serverStart"
-  })
-})
-
+//
 app.use((req, res, next) => {
   res.status(404)
       .json({
