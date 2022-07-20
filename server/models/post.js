8ac7844e22e6fbec717b06191f487b69cc99250d
Mika.dev <charmikadev@gmail.com>
2022-07-20 14:11:58 +0100

    server models

--- server/models/post.js
+++ server/models/post.js
@@ -4,9 +4,10 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
     title: {type: String, required: true},
     text: {type: String, required: true},
-    date_formatted: {type: String, required: true},
     timestamp: {type: Date, required: true},
     user: {type: Schema.Types.ObjectId, ref:'User', required: true},
+    published: {type: Boolean, required: true, default: false}
+
 })
 
 module.exports = mongoose.model('Message', PostSchema);
