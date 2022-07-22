f5840ac0976b480f945af893d1fa4e33a64a6dcf
Mika.dev <charmikadev@gmail.com>
2022-07-22 20:20:27 +0100

    updated front end styling, added svg for homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,14 +2,34 @@ import React from 'react';
 import Nav from "./Nav";
 // eslint-disable-next-line
 import HomeCss from './styles/Home.css';
+import WebFont from "webfontloader";
 
 function Home() {
+    WebFont.load({
+        custom: {
+            families: ['Voga-Medium'],
+            urls:['./fonts/Voga-Medium.otf']
+        }
+    })
+
+    const titleFont = {
+        fontFamily: "Voga-Medium",
+        fontSize: "28px"
+    }
+
   return (
       <div className='app'>
           <Nav />
           <main className='content'>
-              <h1>Home</h1>
-              <p>Hello</p>
+              <div className='content-left'>
+                  <h1 style={titleFont}><strong>Home</strong></h1>
+              </div>
+
+              <div className="content-right">
+                  <div id="home-image">
+
+                  </div>
+              </div>
           </main>
       </div>
   )
