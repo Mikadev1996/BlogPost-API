f547a05058a7f335fd7731af62cb4370ef9f089f
Mika.dev <charmikadev@gmail.com>
2022-07-25 21:31:28 +0100

    Post component css

--- client/src/components/ViewPost.js
+++ client/src/components/ViewPost.js
@@ -10,28 +10,27 @@ const ViewPost = () => {
     return (
         <div className='app'>
             <Nav />
-            <div className='content-header'></div>
             <main className='content'>
-                <div className='container view-post-container'>
-                    <div className='view-post-background'>
-                        <div className='view-post-content'>
-                            <div className='view-post-likes-container'>Vote</div>
+                <div className='view-post-content-header'></div>
+                <div className='view-post-container'>
+                    <div className='view-post-content'>
+                        <div className='view-post-likes-container'>Vote</div>
 
-                            <div className='view-post-info'>
-                                <p>Posted by user/SomeUser123 (Date posted)</p>
-                                <div className='view-post-header'>Title</div>
-                            </div>
-
-                            <div className='view-post-text'>
-
-                            </div>
-
-                            <div className='add-comment-container'>
-
-                            </div>
+                        <div className='view-post-info'>
+                            <p>Posted by user/SomeUser123 (Date posted)</p>
+                            <div className='view-post-header'>Title</div>
                         </div>
-                        <Comments postid={postid}/>
+
+                        <div className='view-post-text'>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur cupiditate, distinctio ducimus ea earum hic, incidunt itaque iure laboriosam laudantium magnam nam optio placeat possimus qui saepe sequi tempora voluptate.
+                        </div>
+
+                        <div className='add-comment-container'>
+
+                        </div>
                     </div>
+                    <hr className='hr-comments'/>
+                    <Comments postid={postid}/>
                 </div>
             </main>
             <footer>
