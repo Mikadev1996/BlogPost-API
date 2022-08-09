825dae68a0d37ee1144cbb1703a0dfadd0b18915
Mika.dev <charmikadev@gmail.com>
2022-08-09 22:37:37 +0100

    files edit for heroku deployment

---
+++ client/build/precache-manifest.79357cc18d7c4b809b5a850da96cd203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bf784f3b9ef7361ff66",
+    "url": "/static/css/main.8f3d7c0f.chunk.css"
+  },
+  {
+    "revision": "3bf784f3b9ef7361ff66",
+    "url": "/static/js/main.3bf784f3.chunk.js"
+  },
+  {
+    "revision": "e982651babc1e213ca08",
+    "url": "/static/js/1.e982651b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a106c810528ccbe31c044edfe5abe3fa",
+    "url": "/static/media/undraw_social_update.a106c810.svg"
+  },
+  {
+    "revision": "9e3020aaff4eaea26853e908ce660fd8",
+    "url": "/index.html"
+  }
+];
